ed9bf7fae0d60467b0def16531780feea600defa
Nicolás Zagal Vallejo <u20201c429@upc.edu.pe>
2025-07-08 14:29:46 -0500

    fix(): Local Instance

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       '^/api': {
-        target: 'https://kitchen-tech-backend.onrender.com/api/kitchentech/v1',
+        target: 'http://localhost:8080/api/kitchentech/v1',
         changeOrigin: true
       }
     }
