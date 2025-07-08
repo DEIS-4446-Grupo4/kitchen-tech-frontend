cd6dc9774e52eeb9bd29451d11800b6391556eba
Nicolás Zagal Vallejo <u20201c429@upc.edu.pe>
2025-07-08 07:44:14 -0500

    axios config

--- src/public/services/axiosConfig.js
+++ src/public/services/axiosConfig.js
@@ -1,8 +1,8 @@
 // src/public/services/axiosConfig.js
 import axios from "axios";
 
-//#const API_URL = 'https://kitchen-tech-fqghavb0fychfkhm.brazilsouth-01.azurewebsites.net/api/kitchentech/v1'; // URL base
-const API_URL = 'http://localhost:8081/api/kitchentech/v1'; // URL base
+const API_URL = 'https://kitchen-tech-backend.onrender.com'; // URL base
+//const API_URL = 'http://localhost:8081/api/kitchentech/v1'; // URL base
 
 const axiosInstance = axios.create({
     baseURL: API_URL,
--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       '^/api': {
-        target: 'https://kitchen-tech-fqghavb0fychfkhm.brazilsouth-01.azurewebsites.net',
+        target: 'https://kitchen-tech-backend.onrender.com',
         changeOrigin: true
       }
     }
