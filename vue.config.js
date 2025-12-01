9666acce260347b459b932ca1f1c6cdf82562bc5
DarkArcher822 <112427427+nicolaszagal@users.noreply.github.com>
2025-12-01 00:06:26 -0500

    fix(): config

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,19 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: '/',
   transpileDependencies: true,
   devServer: {
+    historyApiFallback: true,
     proxy: {
       '^/api': {
         target: 'https://kitchen-tech-backend.onrender.com',
         changeOrigin: true,
       }
     },
+  },
+  configureWebpack: {
+    devServer: {
+      historyApiFallback: true
+    }
   }
 })
